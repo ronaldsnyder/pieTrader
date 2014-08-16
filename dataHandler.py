877473b9255078cab4a257bd8032d3f783282065
ronaldsnyder <ronalddsnyder@gmail.com>
2014-08-15 22:15:09 -0400

    adding symbol logic

--- dataHandler.py
+++ dataHandler.py
@@ -27,6 +27,8 @@ class DataHandler:
         conn.commit()
         conn.close()
 
+
+
     def get_last_update_date(self):
         pass
 
@@ -45,6 +47,30 @@ class DataHandler:
         conn.close()
 
     @staticmethod
+    def create_symbol_table():
+        conn = sqlite3.connect('data/pietrader.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE symbols(symbol_id integer primary key autoincrement, symbol)''')
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def drop_symbol_table():
+        conn = sqlite3.connect('data/pietrader.db')
+        c = conn.cursor()
+        c.execute('''DROP TABLE symbols''')
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def update_symbols_table(symbol):
+        conn = sqlite3.connect('data/pietrader.db')
+        c = conn.cursor()
+        c.execute('INSERT INTO symbols (symbol) VALUES (?)', (symbol,))
+        conn.commit()
+        conn.close
+
+    @staticmethod
     def get_data_test(code):
         conn = sqlite3.connect('data/pietrader.db')
         c = conn.cursor()
@@ -54,19 +80,24 @@ class DataHandler:
     @staticmethod
     def init_load():
         #initial load of data to the table
+        DataHandler.drop_symbol_table()
+        print "This may take a while..."
+        print "Creating symbols table..."
+        DataHandler.create_symbol_table()
+        print "Fetching Stock Symbols from Quandl..."
         qdl = quandl.Quandl()
         symbols = qdl.get_symbols()
+        print "Adding the symbols to the database..."
         for symbol in symbols:
-            print "Adding data for: %s" % symbol
-            #stock = qdl.get_stock(symbol)
-            DataHandler.update_stock_table(symbol)
+            print "Adding symbol: %s" % symbol
+            DataHandler.update_symbols_table(symbol)
 
     @staticmethod
     def create_favorite_table():
         conn = sqlite3.connect('data/pietrader.db')
         c = conn.cursor()
         c.execute('''CREATE TABLE favorites
-                    (id integer primary key autoincrement, symbol, name)''')
+                    (favorite_id integer primary key autoincrement, symbol, name)''')
         conn.close()
 
     def set_favorites(self):
