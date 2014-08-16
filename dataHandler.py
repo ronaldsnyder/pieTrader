5a54c4e2c96536d60cd7a3b7deeeaba73bd1a3ef
ronaldsnyder <ronalddsnyder@gmail.com>
2014-08-16 13:42:42 -0400

    made database connection instance variable and removed static methods

--- dataHandler.py
+++ dataHandler.py
@@ -7,97 +7,94 @@ import quandl
 
 class DataHandler:
 
+    conn = None
+    c = None
+    qdl = None
+
     def __init__(self):
         self.last_update = ''
+        self.conn = sqlite3.connect('data/pietrader.db')
+        self.c = self.conn.cursor()
+        self.qdl = quandl.Quandl()
 
-    @staticmethod
-    def create_stock_table():
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS stock
+    def __del__(self):
+        self.conn.close()
+
+    def create_stock_table(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS stock
                     (symbol, date, open, high, low, close, volume, ex_dividend, split_ration,
                     adj_open, adj_high, adj_low, adj_close, adj_volume)''')
-        conn.close()
+        self.conn.commit()
 
-    @staticmethod
-    def drop_stock_table():
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        c.execute('''DROP TABLE iF EXISTS stock''')
-        conn.commit()
-        conn.close()
+    def drop_stock_table(self):
+        self.c.execute('''DROP TABLE iF EXISTS stock''')
+        self.conn.commit()
 
     def get_last_update_date(self):
         pass
 
-    @staticmethod
-    def update_stock_table(**kwargs):
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        c.execute('''INSERT INTO stock
+    def update_stock_table(self, **kwargs):
+        print "Adding stock data for: %s %s" % (kwargs["symbol"], kwargs["Date"])
+        self.c.execute('''INSERT INTO stock
                     (symbol, date, open, high, low, close, volume, ex_dividend, split_ration,
                     adj_open, adj_high, adj_low, adj_close, adj_volume) VALUES
                     (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
                     (kwargs["symbol"], kwargs["Date"], kwargs["Open"], kwargs["High"], kwargs["Low"], kwargs["Close"],
                      kwargs["Volume"], kwargs["Ex-Dividend"], kwargs["Split Ratio"], kwargs["Adj. Open"],
                      kwargs["Adj. High"], kwargs["Adj. Low"], kwargs["Adj. Close"], kwargs["Adj. Volume"]))
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
-    @staticmethod
-    def create_symbol_table():
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS symbols(symbol_id integer primary key autoincrement, symbol)''')
-        conn.commit()
-        conn.close()
+    def create_symbol_table(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS symbols(symbol_id integer primary key autoincrement, symbol)''')
+        self.conn.commit()
 
-    @staticmethod
-    def drop_symbol_table():
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        c.execute('''DROP TABLE IF EXISTS symbols''')
-        conn.commit()
-        conn.close()
+    def drop_symbol_table(self):
+        self.c.execute('''DROP TABLE IF EXISTS symbols''')
+        self.conn.commit()
 
-    @staticmethod
-    def update_symbols_table(*symbols):
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
+    def update_symbols_table(self, *symbols):
         for symbol in symbols:
             print "Adding symbol: %s" % symbol
-            c.execute('INSERT INTO symbols (symbol) VALUES (?)', (symbol,))
-        conn.commit()
-        conn.close
+            self.c.execute('INSERT INTO symbols (symbol) VALUES (?)', (symbol,))
+        self.conn.commit()
 
-    @staticmethod
-    def get_data_test(code):
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        results = c.execute('''SELECT * FROM symbols where symbol = ?''', (code,))
+    def get_data_test(self, symbol):
+        results = self.c.execute('''SELECT * FROM symbols where symbol = ?''', (symbol,))
         print results.fetchone()
 
-    @staticmethod
-    def init_load():
+    def init_load(self):
         #initial load of data to the table
-        DataHandler.drop_symbol_table()
+        self.drop_symbol_table()
         print "This may take a while..."
         print "Creating symbols table..."
-        DataHandler.create_symbol_table()
+        self.create_symbol_table()
+
         print "Fetching Stock Symbols from Quandl..."
-        qdl = quandl.Quandl()
-        symbols = qdl.get_symbols()
+        symbols = self.qdl.get_symbols()
+
         print "Adding the symbols to the database..."
-        DataHandler.update_symbols_table(*symbols)
+        self.update_symbols_table(*symbols)
+
+        print "Symbols have been added..."
+        print "Creating stock table..."
+        self.drop_stock_table()
+        self.create_stock_table()
+
+        print "Retrieving stock info..."
+        print "This may be a good time to read the documentation..."
+        for symbol in symbols:
+            stock_data = self.qdl.get_stock_by_date(symbol, '2014-08-10', '2014-08-15')
+            for data in stock_data:
+                self.update_stock_table(**data)
+
+        print "Initial Load Complete"
 
 
-    @staticmethod
-    def create_favorite_table():
-        conn = sqlite3.connect('data/pietrader.db')
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS favorites
+
+    def create_favorite_table(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS favorites
                     (favorite_id integer primary key autoincrement, symbol, name)''')
-        conn.close()
+        self.conn.commit()
 
 
 
